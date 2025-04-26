9b9a3c085371a4d48d4f42026050c4e76a28da7f
michael10 <qiningshi@gmail.com>
2025-04-26 22:28:59 +0800

    aaa

--- time_mcp_server.py
+++ time_mcp_server.py
@@ -21,22 +21,32 @@ async def get_current_datetime():
     """获取当前时间
 
     Returns:
-        str: 当前时间，返回格式yyyy-MM-dd HH:mm:ss，如2025-04-23 12:00:00
+        str: 当前时间，返回格式yyyy-MM-dd HH:mm:ss，示例 2025-04-23 12:00:00
     """
     logging.info('获取当前时间')
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 @mcp.tool()
-async def get_weather(datetime: str = None):
-    """查询天气
-
+async def get_devices_by_room(room_name: str = None):
+    """根据房间名称查询设备列表
     Args:
-        datetime (str, optional): 日期时间，格式yyyy-MM-dd HH:mm:ss，如2025-04-23 12:00:00
+        room_name (str, optional): 房间名称，示例：302
 
     Returns:
-        str: 内容包含天气，风速风向，空气质量
+        str: 房间中设备列表，
+        示例：
+        设备列表如下：
+        --- did=111, device_name=吸顶灯, room_name=302, gateway_did=g999
+        --- did=g999, device_name=蓝牙网关, room_name=302
+
     """
-    return '天气晴，北风4级，空气质量优'
+    device_str_list = [
+        'did=111, device_name=吸顶灯, room_name=302, gateway_did=g999, online=false',
+        'did=222, device_name=窗帘, room_name=302, gateway_did=g999, online=false',
+        'did=333, device_name=走廊灯, room_name=302, gateway_did=g999, online=false',
+        'did=g999, device_name=蓝牙网关, room_name=302, online=false'
+    ]
+    return '设备列表如下：' + '\n--- '.join(device_str_list)
 
 
 if __name__ == "__main__":
