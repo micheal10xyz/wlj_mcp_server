65dab03a17ae280f31090ace037dd42cad23ec15
bymiot <it@bymiot.com>
2025-05-06 14:58:49 +0800

    111

--- time_mcp_server.py
+++ time_mcp_server.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     )
 
 
-mcp = FastMCP('time')
+mcp = FastMCP('smart-hotel')
 
 @mcp.tool()
 async def get_current_datetime():
@@ -25,6 +25,36 @@ async def get_current_datetime():
     """
     logging.info('获取当前时间')
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+@mcp.tool()
+async def get_history_properties(did: str = None):
+    """根据设备ID获取历史属性
+
+    Returns:
+        List[str]: 设备历史属性
+        格式描述：
+        ---time表示发生时间
+        ---did表示设备ID
+        ---property表示属性名称
+        ---value表示属性值
+        返回值示例：
+        [
+            "time=2025-04-28 12:00:00, did=111, property=开关，value=true",
+            "time=2025-04-28 12:00:00, did=111, property=开关，value=false",
+            "time=2025-04-29 12:00:00, did=111, property=开关，value=true"
+        ]
+    """
+    logging.info('获取历史属性')
+    history_properties = [
+        "time=2025-04-28 12:00:00, did=111, property=开关，value=true",
+        "time=2025-04-28 12:00:00, did=111, property=开关，value=false",
+        "time=2025-04-29 12:00:00, did=111, property=开关，value=true",
+        "time=2025-05-01 12:00:00, did=111, property=开关，value=false",
+        "time=2025-05-06 12:00:00, did=111, property=开关，value=true"
+    ]
+    return history_properties
+
+
 
 @mcp.tool()
 async def get_devices_by_room(room_name: str = None):
@@ -40,6 +70,7 @@ async def get_devices_by_room(room_name: str = None):
         --- did=g999, device_name=蓝牙网关, room_name=302
 
     """
+    logging.info('获取设备列表')
     device_str_list = [
         'did=111, device_name=吸顶灯, room_name=302, gateway_did=g999, online=false',
         'did=222, device_name=窗帘, room_name=302, gateway_did=g999, online=false',
